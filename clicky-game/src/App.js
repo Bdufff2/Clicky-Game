8c81224e1aa89f89ac2281f96a1b69de18c3cff4
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-02-05 18:46:53 -0800

    Reset game files

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
-import './components/Game';
-import './components/Scoreboard';
+import Game from './components/Game';
+import Navbar from'./components/Navbar';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-const App = () => <Game />;
+class App extends React.Component {
+    state = {
+        score: 0,
+        topScore: 0
+    };
+    increaseScore = () => {
+        this.setState({
+            score: this.state.score + 1
+        }) 
+    }
 
+    
+    render(){
+        return (
+            
+          <div>
+              <Navbar score={this.state.score} topScore={this.state.topScore} />
+              <Game increaseScore={this.increaseScore} />
+                    
+            <Wrapper>
+              <Title>Friends List</Title>
+              {this.state.friends.map(friend => (
+                <FriendCard
+                  removeFriend={this.removeFriend}
+                  id={friend.id}
+                  key={friend.id}
+                  name={friend.name}
+                  image={friend.image}
+                  occupation={friend.occupation}
+                  location={friend.location}
+                />
+              ))}
+            </Wrapper>
+          </div>
+       )
+    }
+}
 export default App;
